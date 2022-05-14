dd24880e9fc40a7182dacc41a431c71b0a0aa28c
florence96 <projin1234@gmail.com>
2022-05-15 02:33:45 +0900

    도서목록 - Naver Open API 사용

--- BookList2/BookList2/ViewController.swift
+++ BookList2/BookList2/ViewController.swift
@@ -12,24 +12,6 @@ class ViewController: UIViewController, BookListCellDelegate {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var pageTitle: UILabel!
     
-//    var bookData = [
-//        Book(name: "꼼꼼한 재은 씨의 스위프트 문법편", detail: "이재은 (지은이)", thumbnail: "book_1"),
-//        Book(name: "꼼꼼한 재은 씨의 스위프트 기본편", detail: "이재은 (지은이)", thumbnail: "book_2"),
-//        Book(name: "꼼꼼한 재은 씨의 스위프트 실전편", detail: "이재은 (지은이)", thumbnail: "book_3"),
-//        Book(name: "Do it! 자바 프로그래밍 입문", detail: "박은종 (지은이)", thumbnail: "book_4"),
-//        Book(name: "Do it! 자료구조와 함께 배우는 알고리즘 입문: 자바 편", detail: "시바타 보요 (지은이)", thumbnail: "book_5"),
-//        Book(name: "Do it! 핸즈온 머신러닝", detail: "오렐리앙 제롱 (지은이)", thumbnail: "book_6"),
-//        Book(name: "내가 틀릴 수도 있습니다", detail: "비욘 나티코 린데블라드 (지은이)", thumbnail: "book_7"),
-//        Book(name: "작별인사", detail: "김영하 (지은이)", thumbnail: "book_8"),
-//        Book(name: "오늘부터 개발자", detail: "김병욱 (지은이)", thumbnail: "book_9"),
-//        Book(name: "유지보수하기 어렵게 코딩하는 방법: 평생 개발자로 먹고 살 수 있다", detail: "로에디 그린 (지은이)", thumbnail: "book_10"),
-//        Book(name: "오늘도 개발자가 안 된다고 말했다", detail: "김중철, 김수지 (지은이)", thumbnail: "book_11"),
-//        Book(name: "면접을 위한 CS 전공지식 노트", detail: "주홍철 (지은이)", thumbnail: "book_12"),
-//        Book(name: "나는 LINE 개발자입니다", detail: "김윤신, 김영환 외 (지은이)", thumbnail: "book_13"),
-//        Book(name: "여름의 문", detail: "가와카미 미에코 (지은이)", thumbnail: "book_14"),
-//        Book(name: "도메인 주도 개발 시작하기", detail: "최범균 (지은이)", thumbnail: "book_15")
-//    ]
-    
     var bookData = [Book]()
     var books = [Book]()
     var status = [Bool]()
